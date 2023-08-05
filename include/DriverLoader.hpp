95440dcc43878f8bb0a940bdd8c5b62efda55c1b
Nitr0-G <120374383+Nitr0-G@users.noreply.github.com>
2023-08-05 04:53:54 +0300

    Main files

---
+++ include/DriverLoader.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <Windows.h>
+class DRIVER {
+private:
+    LPTSTR mDriverPath, mServiceName, mDosServiceName;
+    DWORD mStartType;
+    SC_HANDLE mhService;
+private:
+    bool Init, Loaded, Started;
+private:
+    inline bool IsInit() const { return Init; }
+    inline bool IsLoaded() const { return Loaded; }
+    inline bool IsStarted() const { return Started; }
+
+public:
+    DWORD UnloadSvc();
+    DWORD StopSvc();
+    DWORD StartSvc();
+    DWORD CreateSvc();
+    inline DWORD InitSvc(LPTSTR DriverPath, LPTSTR ServiceName, LPTSTR DosServiceName, DWORD StartType);
+public:
+    inline void LoadDriver(LPTSTR DriverPath, LPTSTR ServiceName, LPTSTR DosServiceName, DWORD StartType);
+    inline void UnloadDriver();
+};
